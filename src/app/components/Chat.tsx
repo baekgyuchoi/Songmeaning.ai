c15d29e94c76558ecb42c137a2757039fe9bd5ba
tonychoi3580 <tony.baekgyu.choi@gmail.com>
2023-10-27 16:27:05 -0700

    initial Setup
    Installed Shadcn UI with accordion

---
+++ src/app/components/Chat.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+
+/**
+ * Chat component props
+ * to be implemented with Song and Artist Info
+ */
+interface ChatProps {
+}
+
+/**
+ * Chat component
+ * @param props Chat component props
+ * @returns Chat component UI
+ */
+const Chat: FC<ChatProps> = ({}) => {
+    return (
+        <div>
+            CHAT
+        </div>
+    );
+};
+
+export default Chat;
