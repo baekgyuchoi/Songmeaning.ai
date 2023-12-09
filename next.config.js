27d9c95c5f2946995f1c4e8a85b760bd5f50fb17
tonychoi3580 <tony.baekgyu.choi@gmail.com>
2023-12-08 20:05:00 -0800

    test

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack(config, { isServer, dev }) {
-        config.experiments = {
-          asyncWebAssembly: true,
-          layers: true,
-        };
-    
-        return config;
-      },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
