a2316da1a50fc60f3aa775a2555dfa38762e2f19
tonychoi3580 <tony.baekgyu.choi@gmail.com>
2023-12-16 00:16:46 -0800

    speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Chat from './components/(chat-components)/Chat'
 import Providers from './components/(chat-components)/Providers'
 import { NextUIProviderWrapper } from './components/NextUIProviderWrapper'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           {children}
         </NextUIProviderWrapper>
         <Analytics />
+        <SpeedInsights />
         </body>
       </Providers>
       
