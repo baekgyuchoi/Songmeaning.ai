5523527727f55e41667b63ffbe8c7327a06545ca
tonychoi3580 <tony.baekgyu.choi@gmail.com>
2023-12-16 01:15:19 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import FooterContainer from "./components/(footer)/FooterContainer";
 export default async function Home() {
 
   return (
-    <div>
+    <div className="min-h-screen">
       <main className="flex flex-col items-center justify-between p-4 pt-16 pb-16 lg:pt-0 lg:pb-0 lg:p-8">
         <div className='mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 mt-5 sm:mt-20 mb-0 flex w-full flex-1 flex-col pl-0 pr-0 items-center'>
           <div className="flex items-center flex-col container w-full md:w-3/5 pb-16  " >
