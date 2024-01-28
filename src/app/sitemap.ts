717556447685280831605ac9f0f501fe8162d3b2
tonychoi3580 <tony.baekgyu.choi@gmail.com>
2024-01-27 23:59:18 -0800

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,11 +4,15 @@ import prisma from "@/lib/db";
 const URL = "https://www.songmeaning.ai";
  
 export default async function sitemap() {
-    const songs = await prisma.songMeaning.findMany({})
+    const songs = await prisma.songs.findMany({
+      include: {
+        song_meaning_preview: true,
+      }
+    })
     const artists = await prisma.artist.findMany({})
-    const song_pages = songs.map(({ slug, createdAt }) => ({
-        url: `${URL}/songs/${slug}`,
-        lastModified: createdAt,
+    const song_pages = songs.map(({ song_slug, song_meaning_preview }) => ({
+        url: `${URL}/songs/${song_slug}`,
+        lastModified: song_meaning_preview?.createdAt || new Date().toISOString(),
     
     }))
 
