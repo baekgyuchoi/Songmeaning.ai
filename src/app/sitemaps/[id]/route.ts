b9f4134444b54fc7be5a80afaa6711f5bd749968
tonychoi3580 <tony.baekgyu.choi@gmail.com>
2024-03-07 01:58:23 -0800

    sitemap update

--- src/app/sitemaps/[id]/route.ts
+++ src/app/sitemaps/[id]/route.ts
@@ -49,7 +49,7 @@ export async function GET(request: Request) {
 
       const pageNumber = parseInt(currentPage.split('/').pop() || '0');
       
-      if (pageNumber < 0 || pageNumber > 300) {
+      if (pageNumber < 0 || pageNumber > 5) {
         return new Response('Invalid page number', { status: 400 });
       }
   
