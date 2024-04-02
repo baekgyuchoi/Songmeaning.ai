f9d25f4a88969ea68e08e609734ed7216dd15795
tonychoi3580 <tony.baekgyu.choi@gmail.com>
2024-04-02 15:40:03 -0700

    sitemap update

--- src/app/sitemaps_index.xml/route.tsx
+++ src/app/sitemaps_index.xml/route.tsx
@@ -4,7 +4,7 @@ export async function GET() {
     let xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
     <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 
-    for (let i = 1; i <= 50; i++) {
+    for (let i = 1; i <= 64; i++) {
         xmlContent += `
         <sitemap>
             <loc>https://www.songmeaning.ai/sitemaps/${i}.xml</loc>
